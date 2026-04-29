964dcb62712cb77845150f9707411e83db751adc
Min-Thein-Khaing <nosoonerthan1@gmail.com>
2026-04-29 21:30:04 +0700

    nav tag

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,20 +2,35 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
+import { useEffect } from 'react';
+import { useFonts, SpaceMono_400Regular } from '@expo-google-fonts/space-mono';
+import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from '@/hooks/use-color-scheme';
-
+// Splash Screen ကို ခေတ္တရပ်ထားရန်
+SplashScreen.preventAutoHideAsync();
 export const unstable_settings = {
   anchor: '(tabs)',
 };
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-
+  // Mono Font ကို load လုပ်ခြင်း
+  const [loaded, error] = useFonts({
+    'SpaceMono': SpaceMono_400Regular,
+  });
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack> 
+        <Stack.Screen name="index" />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(setting)"  />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
